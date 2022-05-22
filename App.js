1e7c733db4f464432a0f8da0890b7c5d8ee74090
shafin_shant0 <shafinshanto9@gmail.com>
2022-05-22 15:13:39 +0600

    set up typography folder initaialize project structure adding fonts

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/themes/colors';
 import { spacing } from './src/themes/spacing';
 
 export default function App() {
+
+  const [loader] = useFonts({ //Loading Require Fonts
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'LeagueSpartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'LeagueSpartan-Regular' : require('./assets/fonts/LeagueSpartan-Regular.ttf'),
+  })
+  if (!loader) {
+    return (<Text>Font is loading ....</Text>);
+  }
   return (
     <View style={styles.container}>
       <Text>Hey Hello React Native</Text>
-      <Text style={{marginTop: spacing[8]}} >Here Using Spacing</Text>
+      <Text style={{marginTop: spacing[2], fontFamily:'LeagueSpartan-Regular', fontSize: spacing[8]}} >Here Using Spacing</Text>
       <StatusBar style="auto" />
     </View>
   );
