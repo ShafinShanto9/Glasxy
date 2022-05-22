354b617c3f52874c393549a730ff9351e56dfba0
shafin_shant0 <shafinshanto9@gmail.com>
2022-05-22 14:45:40 +0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/themes/colors';
+import { spacing } from './src/themes/spacing';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey Hello React Native</Text>
+      <Text style={{marginTop: spacing[8]}} >Here Using Spacing</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.orange,
     alignItems: 'center',
     justifyContent: 'center',
   },
